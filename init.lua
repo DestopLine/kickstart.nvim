24d043d07f1da110ee429252773c59e22e3a41bd
DestopLine <50529879+DestopLine@users.noreply.github.com>
2024-01-15 18:38:56 -0400

    Add keymap for vim-fugitive Git

--- init.lua
+++ init.lua
@@ -375,6 +375,9 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
+-- Git fugitive keymaps
+vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = 'Git Status' })
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
