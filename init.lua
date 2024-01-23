7ea2cbc7861e56acde4bd3a5f12a3667af449719
DestopLine <50529879+DestopLine@users.noreply.github.com>
2024-01-23 16:19:55 -0400

    Add clangd to `servers` table

--- init.lua
+++ init.lua
@@ -832,12 +832,12 @@ require('mason-lspconfig').setup()
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  -- clangd = {},
   -- gopls = {},
   -- rust_analyzer = {},
   -- tsserver = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
 
+  clangd = {},
   pyright = {},
   lua_ls = {
     Lua = {
